c830e469cf118886c79a5f12de504f61e3783c27
Dominick Vaske <dominickvaske@gmail.com>
2025-12-29 18:31:49 -0500

    Added functionality to have agent add tasks to Todoist via api - Also included memory/history for the agent to remember previous interactions

--- main.py
+++ main.py
@@ -11,22 +11,25 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
 from langchain.agents import create_openai_tools_agent, AgentExecutor
 
+from todoist_api_python.api import TodoistAPI
+
 load_dotenv()
 
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
+todoist = TodoistAPI(todoist_api_key)
+
 # Functions make a chatbot into an agent
 @tool
-def add_task(task):
+def add_task(task, desc=None):
     """
     Add a new task to the user's task list.
     Use this when the user wants to add or create a task
     :return:
     """
-    # todoist_api_code eventually
-    print(task)
-    print("Task added")
+    todoist.add_task(content=task,
+                     description=desc)
 
 #initialize the llm
 tools = [add_task]
@@ -39,11 +42,12 @@ llm = ChatGoogleGenerativeAI(
 
 #initialize prompt for the llm
 system_prompt = "You are a helpful assistant. You will help the user add tasks."
-user_input = "Add a new task to buy milk"
+
 prompt = ChatPromptTemplate([
     ("system", system_prompt),
-    ("user", user_input),
-    MessagesPlaceholder("agent_scratchpad") #will hold intermediate agent messages
+    MessagesPlaceholder("history"), #should be specifically after system prompt
+    ("user", "{input}"),
+    MessagesPlaceholder("agent_scratchpad"), #will hold intermediate agent messages
 ])
 
 # chain used to provide prompt to llm for chat botting
@@ -54,6 +58,21 @@ agent = create_openai_tools_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 # response = chain.invoke({"input":user_input})
-response = agent_executor.invoke({"input":user_input})
 
-print(response)
+# print(response)
+
+history = []
+while True:
+    user_input = input("User: ").strip()
+    if not user_input:
+        continue
+
+    try:
+        response = agent_executor.invoke({"input": user_input,
+                                          "history":history})
+        print(response)
+        history.append(HumanMessage(content=user_input))
+        history.append(AIMessage(content=response["output"]))
+    except Exception as e:
+        print("Error:", e)
+
